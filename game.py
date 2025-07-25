6cdd40ac5b5025da4d8f16629f58e0f6e4aa32e1
1ajthomas248 <1ajthomas248@gmail.com>
2025-07-25 15:08:00 -0500

    config units and Game class created

---
+++ game.py
@@ -0,0 +1,24 @@
+import pygame
+from config import SCREEN_HEIGHT, SCREEN_WIDTH, FPS, WHITE, BLACK, RED, GREEN
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+        
+            self.screen.fill(WHITE)
+
+            pygame.display.flip()
+
+            self.clock.tick(FPS)
+        
+        pygame.quit()
